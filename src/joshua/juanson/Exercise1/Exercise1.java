f7e9a2625d0bbe48d658088e8cb21e7c837f654a
Joshua Juanson <joshua_juanson@dlsu.edu.ph>
2016-07-28 22:54:52 +0800

    Exercise1
    Signed-off-by: Joshua Juanson <joshua_juanson@dlsu.edu.ph>

--- src/joshua/juanson/Exercise1/Exercise1.java
+++ src/joshua/juanson/Exercise1/Exercise1.java
@@ -14,12 +14,12 @@ public class Exercise1 {
     // if n = 5, 5! = 120
 
     // Exercise 1.2
-    int[] nums = {3, 5, 1, 35, 29, 22, 82, 86, 95, 72};
+    int[] num = {3, 5, 1, 35, 29, 22, 82, 86, 95, 72};
     // Print all the odd numbers inside the nums array
     int b=0;
      // Print all the odd numbers inside the nums array
      for(b = 0; b < 9; b++){
-            if (nums[b] % 2 != 0) {
-                System.out.println(nums[b]);
+            if (num[b] % 2 != 0) {
+                System.out.println(num[b]);
   }
 }
